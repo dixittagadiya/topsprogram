c1e2b9c391bffb4c2a266bf2c5de76843a3b84fa
dixittagadiya <139606709+dixittagadiya@users.noreply.github.com>
2023-07-24 09:24:50 +0530

    Add files via upload

---
+++ de941.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i,j;
+    printf("Enter No:");
+    scanf("%d",&a);
+
+    for(i=a; i>=1; i--)
+    {
+    	for(j=i; j>=1; j--)
+    	{
+    		printf("%d",j*j);
+		}
+		printf("\n");
+	}
+   
+}
